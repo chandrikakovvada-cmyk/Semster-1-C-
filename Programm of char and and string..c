e8bc0538dc1502c8d3255a9c2ca57c943fb51fea
Chandrika kovvada <chandrikakovvada@gmail.com>
2025-11-25 16:21:55 +0530

    Add files via upload

---
+++ Programm of char and and string..c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	char str[24];
+	char sentence[100];
+	scanf("%c %s",&ch,str);
+	getchar();
+	scanf("%[^\n]%*c",sentence);
+	printf("%c\n",ch);
+	printf("%s\n",str);
+	printf("%s\n",sentence);
+}
